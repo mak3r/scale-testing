0ee89974701d3b3992141f3f66a4291f68c9fdea
Andrew Gracey <andrew.gracey@suse.com>
2022-07-25 12:33:06 -0700

    Update build-and-import-k3d.sh

--- build-and-import-k3d.sh
+++ build-and-import-k3d.sh
@@ -5,10 +5,14 @@ LOCAL_CONTEXT=local
 MIN_CLUSTER_ID=100
 MAX_CLUSTER_ID=200
 
+SKIP_IMPORT=false
 
 
 for CLUSTER_ID in {seq $MIN_CLUSTER_ID $MAX_CLUSTER_ID}
 do
+
+if [[ $SKIP_IMPORT != "false" ]]
+then
 
 cat << EOF | kubectl --context $LOCAL_CONTEXT apply -f -
 kind: Cluster
@@ -21,12 +25,18 @@ metadata:
 spec: {}
 EOF
 
+fi
+
 k3d cluster create --api-port 0.0.0.0:7$CLUSTER_ID cluster-$CLUSTER_ID
 
+
+if [[ $SKIP_IMPORT != "false" ]]
+then
+
 REG_TOKEN_LINK=`curl --user $RANCHER_API_AUTH $RANCHER_URL/v3/clusters?name=cluster-$CLUSTER_ID |jq ".data[0].links.clusterRegistrationTokens" -r`
-
 IMPORT_YAML_URL=$RANCHER_URL/v3/import/`curl --user $RANCHER_API_AUTH $REG_TOKEN_LINK |jq '[.data[0].token, .data[0].clusterId ] | join("_")' -r`.yaml
-
 kubectl apply --context k3d-store-$CLUSTER_ID -f $IMPORT_YAML_URL
+
+fi
 
 done
