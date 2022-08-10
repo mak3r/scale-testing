b812c8210e95d988078b68b0e1c5c501881c40c2
Mark Abrams <mabrams@helpguest.com>
2022-08-10 14:00:01 -0400

    initial commit with process steps identified

---
+++ scale-test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+RANCHER_URL=$1
+CLUSTERS_PREFIX=$2
+
+# Check that requirements are available
+def check-config() {
+  if 
+}
+
+# Get baselines for CPU and RAM
+
+# Create a virtual cluster
+
+# Create a cluster in Rancher
+
+# Import the virtual cluster
+
+# Check that the cluster is imported and visibile via Rancher
+
+# Check CPU and RAM
+
+# If CPU and RAM are not at threshold, create another cluster
