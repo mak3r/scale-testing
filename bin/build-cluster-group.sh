734bff3fb686a2e2618361c1bd114672ff0288ff
Mark Abrams <mabrams@helpguest.com>
2022-09-07 02:19:48 -0400

    fix final execution on each server using screen

--- bin/build-cluster-group.sh
+++ bin/build-cluster-group.sh
@@ -2,4 +2,5 @@
 
 source /usr/local/bin/test.sh
 
+cd scale-testing
 create-n $1 $2 $3
--- bin/setup-downstream.sh
+++ bin/setup-downstream.sh
@@ -17,26 +17,14 @@ function install-downstreams {
 	ssh ec2-user@$DOWNIP "cd scale-testing && bin/install-helm.sh"
 	ssh ec2-user@$DOWNIP "cd scale-testing && bin/install-vcluster.sh"
 	ssh ec2-user@$DOWNIP "cd scale-testing && make install_scripts"
-	
-	# ssh ec2-user@$DOWNIP "/bin/bash -s" < bin/install-k3s.sh
-	# ssh ec2-user@$DOWNIP "/bin/bash -s" < bin/install-helm.sh
-	# ssh ec2-user@$DOWNIP "/bin/bash -s" < bin/install-vcluster.sh
-	# scp bin/cluster-ops.sh ec2-user@$DOWNIP:~/.
-	# scp bin/scale-test.sh ec2-user@$DOWNIP:~/.
-	# scp bin/test.sh ec2-user@$DOWNIP:~/.
-	# ssh ec2-user@$DOWNIP "sudo cp cluster-ops.sh /usr/local/bin/. && sudo chown ec2-user:users /usr/local/bin/cluster-ops.sh" 
-	# ssh ec2-user@$DOWNIP "sudo cp scale-test.sh /usr/local/bin/. && sudo chown ec2-user:users /usr/local/bin/scale-test.sh"
-	# ssh ec2-user@$DOWNIP "sudo cp test.sh /usr/local/bin/. && sudo chown ec2-user:users /usr/local/bin/test.sh"
 
 	# Start a screen and build the clusters in the background
-	# scp bin/build-cluster-group.sh ec2-user@$DOWNIP:~/. 
-	ssh ec2-user@$DOWNIP "cd scale-testing && screen -dmS vc ./build-cluster-group.sh $END $START $ACCESS_TOKEN"
+	scp bin/build-cluster-group.sh ec2-user@$DOWNIP:~/. 
+	ssh ec2-user@$DOWNIP screen -dmS vc ./build-cluster-group.sh $END $START $ACCESS_TOKEN
 }
 
 for i in "${!DIP[@]}"
 do 
-	if [ "$i" == "0"]; then
-		install-downstreams "${DIP[$i]}" $i $TOKEN; 
-	fi
+	install-downstreams "${DIP[$i]}" $i $TOKEN; 
 done
 
